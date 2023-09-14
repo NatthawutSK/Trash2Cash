6f82875040d49e160f36e073b3d50fb600a01b98
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2023-09-14 22:55:39 +0700

    ranking 100% ???

--- src/app/(tabs)/scoreRanking.tsx
+++ src/app/(tabs)/scoreRanking.tsx
@@ -1,14 +1,165 @@
-import { View, Text } from "react-native";
 import React from "react";
+import {
+	H5,
+	Separator,
+	SizableText,
+	Tabs,
+	YStack,
+	TabsContentProps,
+	Avatar,
+	Stack,
+	SizeTokens,
+	Text,
+	H4,
+	View,
+	Button,
+	XStack,
+	ColorTokens,
+} from "tamagui";
+import { FlatList } from "react-native";
+import { router } from "expo-router";
 
 type Props = {};
+type PeopleProps = {
+	fullname: string;
+	size: SizeTokens;
+	rank: number;
+	point: number;
+	bcol?: ColorTokens | string;
+};
+export const DATA = [
+	{
+		id: "1",
+		name: "test1 surr1",
+		size: "$6",
+		point: 500,
+	},
+	{
+		id: "2",
+		name: "test1 surr1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "3",
+		name: "test1 surr1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "4",
+		name: "test1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "5",
+		name: "Aaaaaaa Gggggg",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "6",
+		name: "test1 surr1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "7",
+		name: "test1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "8",
+		name: "test1 surr1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "9",
+		name: "test1 surr1",
+		size: "$4",
+		point: 200,
+	},
+	{
+		id: "10",
+		name: "test1",
+		size: "$4",
+		point: 200,
+	},
+];
 
-const scoreRanking = (props: Props) => {
-  return (
-    <View>
-      <Text>scoreRanking</Text>
-    </View>
-  );
+const People = ({ fullname, size, rank, point, bcol }: PeopleProps) => {
+	return (
+		<Stack
+			fd={"row"}
+			space={10}
+			marginVertical={10}
+			bc={bcol === undefined ? "green" : bcol}
+			br={20}
+			p={10}
+			jc={"space-between"}
+			onPress={() => {
+				router.push(`/profileRanking/${rank + 1}`);
+			}}
+		>
+			<View space={10} fd={"row"}>
+				<H5 alignSelf="center" color={rank === 0 ? "gold" : "white"}>
+					{rank + 1}
+				</H5>
+				<Avatar circular size={size}>
+					<Avatar.Image
+						accessibilityLabel="Cam"
+						src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
+					/>
+					<Avatar.Fallback backgroundColor="$blue10" />
+				</Avatar>
+				<H5 color={rank === 0 ? "gold" : "white"} alignSelf="center">
+					{fullname}
+				</H5>
+			</View>
+			<H5
+				ac={"flex-end"}
+				color={rank === 0 ? "gold" : "white"}
+				alignSelf="center"
+			>
+				{point}
+			</H5>
+		</Stack>
+	);
 };
 
-export default scoreRanking;
+const Ranking = (props: Props) => {
+	return (
+		<YStack ai={"stretch"} jc={"center"} w={"100%"} h={"100%"}>
+			<XStack pt={30} ac={"center"} ai={"center"} jc={"space-evenly"}>
+				<Button br={20}>All Time</Button>
+				<Button br={20}>This Month</Button>
+			</XStack>
+			<YStack flex={1} p={20}>
+				<FlatList
+					data={DATA}
+					renderItem={({ item, index }) => (
+						<People
+							fullname={item.name}
+							size={item.size}
+							rank={index}
+							point={item.point}
+						/>
+					)}
+					keyExtractor={(item) => item.id}
+				/>
+				<People
+					fullname="Aaaaaaa Gggggg"
+					size="$5"
+					rank={5}
+					point={200}
+					bcol={"#b23b3b"}
+				/>
+			</YStack>
+		</YStack>
+	);
+};
+
+export default Ranking;
---
+++ src/app/profileRanking/[id].tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import {
+	View,
+	H1,
+	H2,
+	Stack,
+	YStack,
+	H3,
+	Avatar,
+	H5,
+	XStack,
+	Square,
+	Text,
+} from "tamagui";
+import { DATA } from "./../(tabs)/scoreRanking";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Leaf, Trash, Trees, Users } from "@tamagui/lucide-icons";
+type Props = {};
+const detailProfile = (props: Props) => {
+	const { id } = useLocalSearchParams();
+	const user = DATA.find((user) => user.id === id);
+	return (
+		<YStack jc={"center"} als={"center"} ai={"center"} space={30} mt={50}>
+			<Stack
+				fd={"row"}
+				space={10}
+				bc={"green"}
+				br={20}
+				p={10}
+				jc={"space-between"}
+			>
+				<View space={10} fd={"row"}>
+					<Avatar circular size={"$6"}>
+						<Avatar.Image
+							accessibilityLabel="Cam"
+							src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
+						/>
+						<Avatar.Fallback backgroundColor="$blue10" />
+					</Avatar>
+					<H5 color={"white"} alignSelf="center">
+						สถิติของคุณ {user?.name}
+					</H5>
+				</View>
+			</Stack>
+			<XStack space={"$4"}>
+				<YStack space="$4">
+					<Square w={160} h={180} br={20} backgroundColor="#a3cc41">
+						<YStack space={"$3"}>
+							<Text>ลด co2 ไปแล้ว</Text>
+							<Text ta={"center"} fos={"$5"}>
+								90709{"     "}
+								<Text fos={"$2"} color={"#d6e898"}>
+									kCO2e
+								</Text>
+							</Text>
+							{/* <MaterialCommunityIcons
+								name="molecule-co2"
+								size={48}
+								color="black"
+							/> */}
+							<Leaf size={40}></Leaf>
+							{/* <Image source={{ uri: co2 }} /> */}
+						</YStack>
+					</Square>
+					<Square w={160} h={130} br={20} bg={"#a3cc41"}>
+						<YStack space={"$1"}>
+							<Text>เทียบเท่าปลูกต้นไม้</Text>
+							<Text ta={"center"} fos={"$5"}>
+								9070{"     "}
+								<Text fos={"$2"} color={"$gray10Light"}>
+									ต้น
+								</Text>
+							</Text>
+							{/* <MaterialCommunityIcons
+								name="molecule-co2"
+								size={48}
+								color="black"
+							/> */}
+							<Trees size={40}></Trees>
+							{/* <Image source={{ uri: co2 }} /> */}
+						</YStack>
+					</Square>
+				</YStack>
+
+				<YStack space={"$4"}>
+					<Square w={140} h={130} br={20} bg={"#a3cc41"}>
+						<YStack space={"$2"}>
+							<Text>ลดขยะ</Text>
+							<Text ta={"center"} fos={"$5"}>
+								9070{"     "}
+								<Text fos={"$2"} color={"$gray10Light"}>
+									กก.
+								</Text>
+							</Text>
+							<Trash size={40}></Trash>
+						</YStack>
+					</Square>
+					<Square w={140} h={180} br={20} bg={"#a3cc41"}>
+						<YStack space={"$2"}>
+							<Text>ผู้เข้าร่วมช่วยโลก</Text>
+							<Text ta={"center"} fos={"$5"}>
+								922{"     "}
+								<Text fos={"$2"} color={"$gray10Light"}>
+									คน
+								</Text>
+							</Text>
+							<Users size={40}></Users>
+						</YStack>
+					</Square>
+				</YStack>
+			</XStack>
+		</YStack>
+	);
+};
+
+export default detailProfile;

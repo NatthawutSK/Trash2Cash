8d860142837c278f0b4461053c332b861facfdfe
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2023-09-14 13:03:02 +0700

    for merge again

--- src/app/(profile)/changePassword.tsx
+++ src/app/(profile)/changePassword.tsx
@@ -1,16 +1,24 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { YStack, Avatar, XStack, Input, TextArea, Button } from "tamagui";
+import { YStack, Avatar, XStack, Input, TextArea, Button, H3 } from "tamagui";
 
 type Props = {};
 
 const changePassword = (props: Props) => {
 	return (
-		<YStack padding={"$4"}>
-			<Input flex={1} h={"$1"} w={"$1"} placeholder="ชื่อ" />
-			<Input flex={1} h={"$1"} w={"$1"} placeholder="นามสกุล" />
-
-			<Button w={"100%"}>บันทึก</Button>
+		<YStack
+			flex={1}
+			padding={"$4"}
+			space="$6"
+			ac={"center"}
+			alignSelf="center"
+			w={"100%"}
+		>
+			<H3 ta={"center"}>เปลี่ยนรหัสผ่าน</H3>
+			<Input placeholder="รหัสผ่านเดิม"></Input>
+			<Input placeholder="รหัสผ่านใหม่"></Input>
+			<Input placeholder="ยืนยันรหัสผ่าน"></Input>
+			<Button>เปลี่ยนรหัสผ่าน</Button>
 		</YStack>
 	);
 };

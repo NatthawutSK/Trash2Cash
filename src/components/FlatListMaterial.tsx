ec4f47ea3c326faad510051fe38a3e79e0c82fc0
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-10-23 23:58:40 +0700

    almost done

--- src/components/FlatListMaterial.tsx
+++ src/components/FlatListMaterial.tsx
@@ -1,32 +1,32 @@
 import { View, Text, FlatList } from "react-native";
 import React from "react";
-import StoreItem, { StoreProps } from "./StoreItem";
+import StoreItem from "./StoreItem";
 import MaterialItem, { MatProps } from "@/components/MaterialItem";
 import { Stack } from "tamagui";
 
 type Props = {};
 
 const renderMaterial = (item: { item: MatProps }) => {
-	return <MaterialItem {...item.item} />;
+  return <MaterialItem {...item.item} />;
 };
 
 const FlatListMaterial = ({ data }: { data: MatProps[] }) => {
-	return (
-		<Stack p={20}>
-			<FlatList
-				contentContainerStyle={{
-					gap: 10,
-					paddingBottom: 80,
-				}}
-				showsVerticalScrollIndicator={false}
-				data={data}
-				renderItem={renderMaterial}
-				keyExtractor={(item, index) => index.toString()}
-				// Performance settings
-				removeClippedSubviews={true}
-			/>
-		</Stack>
-	);
+  return (
+    <Stack p={20}>
+      <FlatList
+        contentContainerStyle={{
+          gap: 10,
+          paddingBottom: 80,
+        }}
+        showsVerticalScrollIndicator={false}
+        data={data}
+        renderItem={renderMaterial}
+        keyExtractor={(item, index) => index.toString()}
+        // Performance settings
+        removeClippedSubviews={true}
+      />
+    </Stack>
+  );
 };
 
 export default FlatListMaterial;

b1bb3ce0ddb981515ea938a5c2cabf738312f512
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2023-10-24 12:36:03 +0700

    Fix Fetch Score, RankingUser Done Fetch Image

--- src/app/(tabs)/scoreRanking.tsx
+++ src/app/(tabs)/scoreRanking.tsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
 import {
-  H5,
-  Separator,
-  SizableText,
-  Tabs,
-  YStack,
-  TabsContentProps,
-  Avatar,
-  Stack,
-  SizeTokens,
-  Text,
-  H4,
-  View,
-  Button,
-  XStack,
-  ColorTokens,
-  validPseudoKeys,
+	H5,
+	Separator,
+	SizableText,
+	Tabs,
+	YStack,
+	TabsContentProps,
+	Avatar,
+	Stack,
+	SizeTokens,
+	Text,
+	H4,
+	View,
+	Button,
+	XStack,
+	ColorTokens,
+	validPseudoKeys,
 } from "tamagui";
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import { router } from "expo-router";
 import RankingUser from "./../../components/RankingUser";
 import FlatlistRanking from "@/components/FlatlistRanking";
@@ -30,219 +30,283 @@ import { Score } from "@/types";
 import { useUserContext } from "@/provider/UserContext";
 import { fetchImgPro, fetchProfile } from "@/utils/util";
 import { UserRankType } from "@/MockData/types";
+import { ScrollView } from "react-native-virtualized-view";
+import { useFocus } from "@/components/UseFocus";
 type Props = {};
 // type UserScore :
 export const DATARank: any[] = [
-  {
-    id: 1,
-    name: "test1 surr1",
-    img: "https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=0",
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 2,
-    name: "test2 surr2",
-    img: "https://picsum.photos/200",
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 3,
-    name: "test3 surr3",
+	{
+		id: 1,
+		name: "test1 surr1",
+		img: "https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=0",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 2,
+		name: "test2 surr2",
+		img: "https://picsum.photos/200",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 3,
+		name: "test3 surr3",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 4,
-    name: "test4",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 4,
+		name: "test4",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 5,
-    name: "Aaaaaaa Gggggg",
-    img: "https://picsum.photos/300",
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 6,
-    name: "test5 surr5",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 5,
+		name: "Aaaaaaa Gggggg",
+		img: "https://picsum.photos/300",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 6,
+		name: "test5 surr5",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 7,
-    name: "test6",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 7,
+		name: "test6",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 8,
-    name: "test7 surr7",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 8,
+		name: "test7 surr7",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 9,
-    name: "tes8 surr8",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 9,
+		name: "tes8 surr8",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
-  {
-    id: 10,
-    name: "test9",
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
+	{
+		id: 10,
+		name: "test9",
 
-    point: {
-      score_trash: 500,
-      score_carbon: 500,
-      score_tree: 500,
-    },
-  },
+		point: {
+			score_trash: 500,
+			score_carbon: 500,
+			score_tree: 500,
+		},
+	},
 ];
 const userPaginatedList = gql`
-  query usersList {
-    usersSellerList(first: 10) {
-      auth_id
-      roles
-      user_name
-      score {
-        score_trash
-        score_carbon
-        score_tree
-      }
-    }
-  }
+	query usersList {
+		usersSellerList(first: 10) {
+			auth_id
+			roles
+			user_name
+			score {
+				score_trash
+				score_carbon
+				score_tree
+			}
+		}
+	}
 `;
 const Ranking = (props: Props) => {
-  const { data, loading, refetch, error } = useQuery(userPaginatedList);
-  const { dbUser }: any = useUserContext();
-  const [img, setImg] = useState<{ name: string; url: string }[]>([]);
-  useEffect(() => {
-    // trigger when you navigate back from another screen
-    // you can background reload data here ...
-    fetchProfile(setImg);
-  }, []);
-  console.log(img, "img");
+	const { data, loading, refetch, error } = useQuery(userPaginatedList);
+	const { dbUser }: any = useUserContext();
+	const [img, setImg] = useState<{ name: string; url: string }[]>([]);
+	const { focusCount, isFocused } = useFocus();
+	// useEffect(() => {
+	// 	// trigger when you navigate back from another screen
+	// 	// you can background reload data here ...
+	// 	fetchProfile(setImg);
+	// }, []);
+	// console.log(img, "img");
+	useEffect(() => {
+		if (focusCount === 1 && isFocused) {
+			// this is the first time focus => init screen here
+			fetchProfile(setImg);
+			refetch();
+		}
+	}, [isFocused]);
 
-  if (loading) {
-    return (
-      <Spinner
-        animation="fade"
-        visible={true}
-        textContent={"Loading..."}
-        textStyle={{ color: "#FFF" }}
-      />
-    );
-  }
-  console.log(error);
-  if (error) return <Text>Something went wrong</Text>;
-  const DATA: UserRankType[] = data?.usersSellerList
-    .slice() // Make a shallow copy of the array
-    .sort(
-      (a: UserRankType, b: UserRankType) =>
-        b.score[0].score_trash - a.score[0].score_trash
-    );
+	useEffect(() => {
+		if (focusCount > 1 && isFocused) {
+			// trigger when you navigate back from another screen
+			// you can background reload data here ...
+			fetchProfile(setImg);
+			refetch();
+		}
+	}, [isFocused]);
 
-  const DATAID = DATA.map((val) => val.auth_id);
+	if (loading) {
+		return (
+			<Spinner
+				animation="fade"
+				visible={true}
+				textContent={"Loading..."}
+				textStyle={{ color: "#FFF" }}
+			/>
+		);
+	}
+	console.log(error);
+	if (error) return <Text>Something went wrong</Text>;
+	//   const DATA: UserRankType[] = data?.usersSellerList
+	//     .slice() // Make a shallow copy of the array
+	//     .sort(
+	//       (a: UserRankType, b: UserRankType) =>
+	//         b.score[0].score_trash - a.score[0].score_trash
+	//     );
 
-  fetchImgPro(DATAID)
-    .then((img: string[]) => {
-      DATA.forEach((val, index) => {
-        val.img = img[index];
-      });
-      // Now that `DATA` is updated with `img` data, you can work with it here.
-      console.log(DATA);
-    })
-    .catch((error) => {
-      console.error("Error fetching images:", error);
-    });
+	//   const DATAID = DATA.map((val) => val.auth_id);
 
-  // console.log(DATA[0], "DATA");
-  return (
-    <YStack bg={"green"} flex={1}>
-      {/* <XStack pt={30} ac={"center"} ai={"center"} jc={"space-evenly"}>
+	//   fetchImgPro(DATAID)
+	//     .then((img: string[]) => {
+	//       DATA.forEach((val, index) => {
+	//         val.img = img[index];
+	//       });
+	//       // Now that `DATA` is updated with `img` data, you can work with it here.
+	//       console.log(DATA);
+	//     })
+	//     .catch((error) => {
+	//       console.error("Error fetching images:", error);
+	//     });
+	let DATA: any = data?.usersSellerList
+		.map((val: any) => {
+			const authId = val.auth_id;
+			const foundImg = img.find((val2) =>
+				val2.name.includes(`${authId}.png`)
+			);
+
+			return {
+				...val,
+				img: foundImg?.url,
+			};
+		})
+		.sort(
+			(a: any, b: any) => b.score[0].score_trash - a.score[0].score_trash
+		);
+	let myImg = img.find((val) =>
+		val.name.includes(`Profile/${dbUser.auth_id}.png`)
+	);
+
+	// console.log(DATA[0], "DATA");
+	return (
+		// <ScrollView
+		// 	style={{ flex: 1 }}
+		// 	refreshControl={
+		// 		<RefreshControl refreshing={loading} onRefresh={refetch} />
+		// 	}
+		// >
+		<YStack bg={"green"} flex={1} jc={"center"} mt={50}>
+			{/* <XStack pt={30} ac={"center"} ai={"center"} jc={"space-evenly"}>
 				<Button
-					bc={"#1cb60e"}
-					color={"#daffd6"}
-					br={20}
+				bc={"#1cb60e"}
+				color={"#daffd6"}
+				br={20}
 					onPress={() => setNow(DATA)}
-				>
+					>
 					All Time
-				</Button>
-				<Button br={20} onPress={() => setNow(DATA2)}>
+					</Button>
+					<Button br={20} onPress={() => setNow(DATA2)}>
 					This Month
-				</Button>
-			</XStack>
-			<YStack flex={1} jc={"center"} ai={"center"} ac={"center"} p={20}>
-				<FlatList
+					</Button>
+					</XStack>
+					<YStack flex={1} jc={"center"} ai={"center"} ac={"center"} p={20}>
+					<FlatList
 					showsVerticalScrollIndicator={false}
 					data={isNow}
 					renderItem={({ item, index }) =>
-						renderRankingUser({ ...item, index: index + 1 })
-					}
-					keyExtractor={(item, index) => index.toString()}
-					// Performance settings
-					removeClippedSubviews={true} // Unmount components when outside of window
-					initialNumToRender={1} // Reduce initial render amount
-					maxToRenderPerBatch={1} // Reduce number in each render batch
-					windowSize={2} // Reduce the window size
-				/> */}
-      <Stack h={dbUser.roles === "Seller" ? "90%" : "100%"} mt={10} mb={15}>
-        <FlatlistRanking data={DATA} />
-      </Stack>
-      {dbUser.roles === "Seller" && (
-        <Stack
-          className="rounded-t-2xl"
-          mt={-50}
-          bg={colors.green3}
-          mb={20}
-          pb={20}
-          // borderRadius={"$10"}
-          pt={"$2"}
-          // mt={"$5"}
-        >
-          <RankingUser {...dbUser} isMe={true} />
-        </Stack>
-      )}
+					renderRankingUser({ ...item, index: index + 1 })
+				}
+				keyExtractor={(item, index) => index.toString()}
+				// Performance settings
+				removeClippedSubviews={true} // Unmount components when outside of window
+				initialNumToRender={1} // Reduce initial render amount
+				maxToRenderPerBatch={1} // Reduce number in each render batch
+				windowSize={2} // Reduce the window size
+			/> */}
+			{/* <Stack
+				h={dbUser.roles === "Seller" ? "90%" : "100%"}
+				mt={10}
+				mb={15}
+			>/ */}
+			{/* <ScrollView
+				style={{ flex: 1 }}
+				refreshControl={
+					<RefreshControl refreshing={loading} onRefresh={refetch} />
+				}
+			> */}
+			<Stack h={dbUser.roles === "Seller" ? "90%" : "105%"} mt={10}>
+				<FlatlistRanking data={DATA} />
+			</Stack>
+			{/* </Stack> */}
+			{dbUser.roles === "Seller" && (
+				<Stack
+					className="rounded-t-2xl"
+					mt={-45}
+					bg={colors.green3}
+					mb={25}
+					pb={40}
+					// borderRadius={"$10"}
+					pt={"$2"}
+					// mt={"$5"}
+				>
+					<RankingUser
+						img={myImg?.url}
+						isMe={true}
+						auth_id={dbUser.auth_id}
+						roles={dbUser.roles}
+						score={dbUser.score}
+						user_name={dbUser.user_name}
+					/>
+				</Stack>
+			)}
+			{/* </ScrollView> */}
 
-      {/* </YStack> */}
-    </YStack>
-  );
+			{/* </YStack> */}
+		</YStack>
+		// </ScrollView>
+	);
 };
 
 export default Ranking;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,9 +4,9 @@ import { ApolloProvider } from "@apollo/client";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 
@@ -33,173 +33,186 @@ import ChooseRole from "@/components/auth/ChooseRole";
 import Spinner from "react-native-loading-spinner-overlay";
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary,
 } from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+	// Ensure that reloading on `/modal` keeps a back button present.
+	initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
-    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
-    Kanit: require("../../assets/fonts/Kanit-Regular.ttf"),
-    ...FontAwesome.font,
-  });
+	const [loaded, error] = useFonts({
+		Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+		InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+		SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+		Kanit: require("../../assets/fonts/Kanit-Regular.ttf"),
+		...FontAwesome.font,
+	});
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return <RootLayoutNavWithProviders />;
+	return <RootLayoutNavWithProviders />;
 }
 
 const tokenCache = {
-  async getToken(key: string) {
-    try {
-      return SecureStore.getItemAsync(key);
-    } catch (err) {
-      return null;
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch (err) {
-      return;
-    }
-  },
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key);
+		} catch (err) {
+			return null;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (err) {
+			return;
+		}
+	},
 };
 
 function RootLayoutNavWithProviders() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <ClerkProvider
-      publishableKey={CLERK_PUBLISHABLE_KEY}
-      tokenCache={tokenCache}
-    >
-      <ApolloProvider client={client}>
-        <UserContextProvider>
-          <TamaguiProvider config={config}>
-            <Theme name={colorScheme}>
-              <ThemeProvider
-                value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-              >
-                <RootLayoutNav />
-              </ThemeProvider>
-            </Theme>
-          </TamaguiProvider>
-        </UserContextProvider>
-      </ApolloProvider>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider
+			publishableKey={CLERK_PUBLISHABLE_KEY}
+			tokenCache={tokenCache}
+		>
+			<ApolloProvider client={client}>
+				<UserContextProvider>
+					<TamaguiProvider config={config}>
+						<Theme name={colorScheme}>
+							<ThemeProvider
+								value={
+									colorScheme === "dark"
+										? DarkTheme
+										: DefaultTheme
+								}
+							>
+								<RootLayoutNav />
+							</ThemeProvider>
+						</Theme>
+					</TamaguiProvider>
+				</UserContextProvider>
+			</ApolloProvider>
+		</ClerkProvider>
+	);
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-  const { authUser, dbUser, loading }: any = useUserContext();
-  console.log(authUser?.id);
-  // console.log(dbUser);
+	const colorScheme = useColorScheme();
+	const { authUser, dbUser, loading }: any = useUserContext();
+	console.log(authUser?.id);
+	// console.log(dbUser);
 
-  if (loading) {
-    return (
-      <Spinner
-        animation="fade"
-        visible={true}
-        textContent={"Loading..."}
-        textStyle={{ color: "#FFF" }}
-      />
-    );
-  }
+	if (loading) {
+		return (
+			<Spinner
+				animation="fade"
+				visible={true}
+				textContent={"Loading..."}
+				textStyle={{ color: "#FFF" }}
+			/>
+		);
+	}
 
-  return (
-    <>
-      <SignedIn>
-        {!dbUser ? (
-          <ChooseRole />
-        ) : (
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-            <Stack.Screen name="(map)/fullMap" options={{ title: "แผนที่" }} />
-            <Stack.Screen
-              name="(map)/mapPick"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(map)/nearbyRanking"
-              options={{ title: "อันดับใกล้เคียง" }}
-            />
-            <Stack.Screen
-              name="search"
-              options={{ title: "ค้นหา", headerShown: false }}
-            />
-            <Stack.Screen
-              name="detailStore/[id]"
-              options={{ title: "Detail Store" }}
-            />
-            <Stack.Screen
-              name="detailTrash/[id]"
-              options={{ title: "Detail Trash" }}
-            />
-            <Stack.Screen
-              name="(profile)/editProfile"
-              options={{ title: "Edit Profile" }}
-            />
-            <Stack.Screen
-              name="(profile)/changePassword"
-              options={{ title: "Change Password" }}
-            />
-            <Stack.Screen
-              name="(profile)/history"
-              options={{ title: "History" }}
-            />
-            <Stack.Screen
-              name="profileRanking/[id]"
-              options={{ title: "User Stat" }}
-            />
-            <Stack.Screen
-              name="qrCodeBuyer"
-              options={{ title: "แสกน QR Code คนขาย" }}
-            ></Stack.Screen>
-            <Stack.Screen
-              name="ApproveDetail"
-              options={{ title: "XD" }}
-            ></Stack.Screen>
-            <Stack.Screen
-              name="(store)/editImageStore"
-              options={{ title: "แก้ไขรูปร้านค้า" }}
-            />
-            <Stack.Screen
-              name="(store)/editMaterialStore/[id]"
-              options={{ title: "แก้ไขวัสดุที่รับ" }}
-            />
-          </Stack>
-        )}
-      </SignedIn>
-      <SignedOut>
-        <Text style={{ fontFamily: "Kanit" }}>ทดสอบ</Text>
-        {/* <AuthScreen /> */}
-      </SignedOut>
-    </>
-  );
+	return (
+		<>
+			<SignedIn>
+				{!dbUser ? (
+					<ChooseRole />
+				) : (
+					<Stack>
+						<Stack.Screen
+							name="(tabs)"
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="modal"
+							options={{ presentation: "modal" }}
+						/>
+						<Stack.Screen
+							name="(map)/fullMap"
+							options={{ title: "แผนที่" }}
+						/>
+						<Stack.Screen
+							name="(map)/mapPick"
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="(map)/nearbyRanking"
+							options={{ title: "อันดับใกล้เคียง" }}
+						/>
+						<Stack.Screen
+							name="search"
+							options={{ title: "ค้นหา", headerShown: false }}
+						/>
+						<Stack.Screen
+							name="detailStore/[id]"
+							options={{ title: "Detail Store" }}
+						/>
+						<Stack.Screen
+							name="detailTrash/[id]"
+							options={{ title: "Detail Trash" }}
+						/>
+						<Stack.Screen
+							name="(profile)/editProfile"
+							options={{ title: "Edit Profile" }}
+						/>
+						<Stack.Screen
+							name="(profile)/changePassword"
+							options={{ title: "Change Password" }}
+						/>
+						<Stack.Screen
+							name="(profile)/history"
+							options={{ title: "History" }}
+						/>
+						<Stack.Screen
+							name="profileRanking/[id]"
+							options={{ title: "User Stat" }}
+						/>
+						<Stack.Screen
+							name="qrCodeBuyer"
+							options={{ title: "แสกน QR Code คนขาย" }}
+						></Stack.Screen>
+						<Stack.Screen
+							name="ApproveDetail"
+							options={{ title: "XD" }}
+						></Stack.Screen>
+						<Stack.Screen
+							name="(store)/editImageStore"
+							options={{ title: "แก้ไขรูปร้านค้า" }}
+						/>
+						<Stack.Screen
+							name="(store)/editMaterialStore/[id]"
+							options={{ title: "แก้ไขวัสดุที่รับ" }}
+						/>
+					</Stack>
+				)}
+			</SignedIn>
+			<SignedOut>
+				{/* <Text style={{ fontFamily: "Kanit" }}>ทดสอบ</Text> */}
+				<AuthScreen />
+			</SignedOut>
+		</>
+	);
 }
